8dba54c558f2eeec39765175139f99e461e5fcd4
fsdiosdfjdspf <74426304+fsdiosdfjdspf@users.noreply.github.com>
2021-11-24 21:20:45 +0200

    new ver

--- kalkulatorCHEEK.py
+++ kalkulatorCHEEK.py
@@ -4,141 +4,30 @@ while True:
     p=(n+m)
     u=(n-m)
     x=(n*m)
-    aboba=(n/m)
-    print ("Считаю")
-    print (
-        )
-    print (
-        )
-    print (
-        )
-    print (
-        )
-    print (
-        )
-    print (
-        )
-    print (
-        )
-    print (
-        )
-    print (
-        )
-    print (
-        )
-    print (
-        )
-    print (
-        )
-    print (
-        )
-
-    print (
-        )
-    print (
-        )
-    print (
-        )
-    print (
-        )
-
-    print (
-        )
-    print (
-        )
-
-    print (
-        )
-    print (
-        )
-    print (
-        )
-
-    print (
-        )
-    print (
-        )
-    print (
-        )
-
-    print (
-        )
-    print (
-        )
-    print (
-        )
-    print (
-        )
-    print (
-        )
-    print (
-        )
-    print (
-        )
-    print (
-        )
-    print (
-        )
-
-    print (
-        )
-    print (
-        )
-    print (
-        )
-
-    print (
-        )
-    print (
-        )
-
-    print (
-        )
-    print (
-        )
-    print (
-        )
-    print (
-        )
-    print (
-        )
-    print (
-        )
-
-    print (
-        )
-    print (
-        )
-    print (
-        )
-    print (
-        )
-    print (
-        )
-
-    print (
-        )
-    print (
-        )
-    print (
-        )
-    print (
-        )
-    print (
-        )
-    print (
-        )
-
-    print (
-        )
-    print (                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 )    
-    print ("Итог")
-    print ("Плюс")
-    print (p)
-    print ("Минус")
-    print (u)
-    print ("Умножить")
-    print (x)
-    print ("Разделить")
-    print (aboba)
+    if m==0:
+        print ("Вычисляю никому не нужную информацию")
+        import os
+        os.system('cls')                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+        print ("Итог")
+        print ("Плюс")
+        print (p)
+        print ("Минус")
+        print (u)
+        print ("Умножить")
+        print (x)
+        print ("На ноль делить нельзя")
+    else:
+        print ("Вычисляю никому не нужную информацию")
+        aboba=(n/m)
+        import os
+        os.system('cls')                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+        print ("Итог")
+        print ("Плюс")
+        print (p)
+        print ("Минус")
+        print (u)
+        print ("Умножить")
+        print (x)
+        print ("Разделить")
+        print (aboba)
     
